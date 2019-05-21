147219df451cb0f051c1af2c98c912fc2c90cf80
Olivier Beaulieu <olivierbeaulieu657@gmail.com>
2019-05-21 17:23:07 +0800

    Fetch zipfile from response, unzip it and read the csv it contains

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 import fetch from 'node-fetch'
 import pino from 'pino'
+import readline from 'readline'
+import request from 'request-promise-native'
+import yauzl from 'yauzl'
 
 const logger = pino({ level: process.env.LOG_LEVEL || 'info' })
 
@@ -7,12 +10,13 @@ function getUrl(cubeId: string) {
   return `https://www150.statcan.gc.ca/t1/wds/rest/getFullTableDownloadCSV/${cubeId}/en`
 }
 
-const API_URL = getUrl('25100063')
+const cubeId = '25100063'
+const API_URL = getUrl(cubeId)
 logger.info(`Fetching ${API_URL}`)
 
 fetch(API_URL)
   .then(response => response.json())
-  .then((response: { status: string; object: string }) => {
+  .then(async (response: { status: string; object: string }) => {
     const { status, object } = response
 
     if (status !== 'SUCCESS') {
@@ -21,4 +25,27 @@ fetch(API_URL)
     }
 
     logger.info(`Fetching object from ${object}`)
+
+    const zipAsBuffer = await request(object, { encoding: null })
+    console.log(zipAsBuffer)
+
+    yauzl.fromBuffer(zipAsBuffer, null, (err, zipfile) => {
+      if (err) throw err
+
+      zipfile.on('entry', entry => {
+        if (entry.fileName === `${cubeId}.csv`) {
+          logger.info(`Opening ${entry.fileName}`)
+          let csvData = ''
+          zipfile.openReadStream(entry, (err, readStream) => {
+            if (err) throw err
+            readStream.on('data', buffer => {
+              csvData += buffer.toString()
+            })
+            readStream.on('end', () => {
+              logger.info(`Finished reading ${entry.fileName}`)
+            })
+          })
+        }
+      })
+    })
   })
